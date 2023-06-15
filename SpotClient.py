f678aeb002446fcb09cff53644a52c3e7f2085f1
Sedinha <luizsperandiodavid@gmail.com>
2023-06-15 17:11:20 -0300

    Update tirando Flood

--- SpotClient.py
+++ SpotClient.py
@@ -3,14 +3,16 @@ import pyaudio
 import time
 import threading
 import json
-#Codigo integralmente feito por Luiz Fernando Sperandio David
+# Codigo integralmente feito por Luiz Fernando Sperandio David
 
 MENSAGEM_FIM_DADOS_MUSICA = "track_data_end"
 TAMANHO_MENSAGEM_FIM_DADOS_MUSICA = len(MENSAGEM_FIM_DADOS_MUSICA)
 
+
 def sendDados(socketCliente, msg: str):
     print(f'Mandando mensagem :"{msg}" para {socketCliente}')
     socketCliente.send(msg.encode())
+
 
 def receberDados(socketCliente):
     msg = ""
@@ -24,14 +26,15 @@ def receberDados(socketCliente):
         print(f'{e}')
     return ''
 
-def baixarMusica(nome_msc,socketCliente):
-    CHUNK = 44100 * 2 * 30 # CHUNK = RATE * CHANNELS * 30 SEGUNDOS
+
+def baixarMusica(nome_msc, socketCliente):
+    CHUNK = 44100 * 2 * 30  # CHUNK = RATE * CHANNELS * 30 SEGUNDOS
     bytesMusica = []
+    cacheLocal[nome_msc.lower()] = bytesMusica
     feito = False
     print(f"RX streaming musica")
     bytes_arquivo = b""
     while not feito:
-        print("RX Musica")
         data_musica = socketCliente.recv(CHUNK)
         bytesMusica.append(data_musica)
         bytes_arquivo += data_musica
@@ -39,19 +42,22 @@ def baixarMusica(nome_msc,socketCliente):
             feito = True
             continue
     print("A musica inteira foi enviada !")
-    cacheLocal[nome_msc.lower()] = bytesMusica
+
 
 def carregarMSC(socketCliente, nome_msc: str):
-	sendDados(socketCliente, f"download {nome_msc}")
-	resp = receberDados(socketCliente)
-	if resp != "track_data_start":
-		return
-	t1 = threading.Thread(target=baixarMusica, args=(nome_msc,socketCliente), daemon=True)
-	t1.start()
+    sendDados(socketCliente, f"download {nome_msc}")
+    resp = receberDados(socketCliente)
+    if resp != "track_data_start":
+        return
+    t1 = threading.Thread(target=baixarMusica, args=(
+        nome_msc, socketCliente), daemon=True)
+    t1.start()
+
 
 cacheLocal = {}
 p = pyaudio.PyAudio()
 listaCacheLocal = []
+
 
 def tocarMusica(nome_msc: str):
     global replay
@@ -96,6 +102,7 @@ def tocarMusica(nome_msc: str):
     stream.stop_stream()
     stream.close()
 
+
 def imprimirBiblioteca():
     linhas = [
         " _____________________________",
@@ -106,12 +113,14 @@ def imprimirBiblioteca():
 
     for linha in linhas:
         print(linha)
-	
+
+
 def getListaMsc(socketCliente):
-	global listaCacheLocal
-	sendDados(socketCliente, 'lista')
-	listaMsc = receberDados(socketCliente)
-	listaCacheLocal = listaMsc.lower().split('\n')
+    global listaCacheLocal
+    sendDados(socketCliente, 'lista')
+    listaMsc = receberDados(socketCliente)
+    listaCacheLocal = listaMsc.lower().split('\n')
+
 
 play = False
 loop = False
@@ -120,10 +129,9 @@ pause = False
 
 
 socketCliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socketCliente.connect(("10.0.0.7",3213))
+socketCliente.connect(("10.0.0.7", 3213))
 nome_cliente = socket.gethostname()
 ip_cliente = socket.gethostbyname(nome_cliente)
-
 
 
 feito = False
@@ -139,7 +147,7 @@ while not feito:
     print("|         9. Change(msc)       10. List_Dipo              |")"
     print("|___________________11. Quit______________________________|")
     '''
-    if comando == "pause" or comando == "6" :
+    if comando == "pause" or comando == "6":
         if play:
             print("Setting paused")
             pause = True
@@ -154,7 +162,7 @@ while not feito:
             play = False
     elif comando in ["restart", "replay"] or comando == "3":
         replay = True
-    elif comando == "stop" or comando == "4" :
+    elif comando == "stop" or comando == "4":
         # TODO use a playing state for a specific thread
         if play:
             play = False
@@ -171,7 +179,8 @@ while not feito:
             if len(listaCacheLocal) == 0:
                 getListaMsc(socketCliente)
             if musica not in listaCacheLocal:
-                print(f"Musica '{musica}' não encontrada. Please send 'lista' para ver a lista de musicas")
+                print(
+                    f"Musica '{musica}' não encontrada. Please send 'lista' para ver a lista de musicas")
                 continue
             if not musica in cacheLocal:
                 print(f"Loading track '{musica}'")
@@ -179,7 +188,8 @@ while not feito:
             print(f"Tocando '{musica}'")
             sendDados(socketCliente, f"att_status {musica}")
             time.sleep(3.0)
-            t=threading.Thread(target=tocarMusica, args=(musica,), daemon=True)
+            t = threading.Thread(target=tocarMusica,
+                                 args=(musica,), daemon=True)
             t.start()
         else:
             print("Nenhuma musica selecionada. Por favor colocar o nome da musica.")
@@ -193,7 +203,7 @@ while not feito:
     elif comando == "quit" or comando == "11":
         print(f"Sending {comando} to server")
         sendDados(socketCliente, comando)
-        resp=receberDados(socketCliente)
+        resp = receberDados(socketCliente)
         if not resp:
             continue
         elif resp.lower() == "goodbye":
@@ -204,23 +214,24 @@ while not feito:
         else:
             print(resp)
     elif comando == "lista_dispositivos" or comando == "10":
-      sendDados(socketCliente, 'lista_dispositivos')
-      devices = receberDados(socketCliente)
-      print("\n")
-      print("Dispositivos: \n")
-      print(devices)
-      indice_escolhido = input('\nEscolha um indice para tocar a musica de um cliente ou (N) para voltar ao menu.\nEscolha um indice:  ').lower()
-      if indice_escolhido == "n":
-        continue
-      elemento_escolhido = devices.split('\n')[int(indice_escolhido) - 1]
-      elemento_dividido = elemento_escolhido.split("Musica: ")
-      musica = elemento_dividido[1]
-      if play:
-          play = False
-          print("Digite Play ou '1' no menu para tocar a musica.")
-      else:
-          print("Digite Play ou '1' no menu para tocar a musica.")
-	#elif comando == "play(msc)_ip" or comando == "11":    
+        sendDados(socketCliente, 'lista_dispositivos')
+        devices = receberDados(socketCliente)
+        print("\n")
+        print("Dispositivos: \n")
+        print(devices)
+        indice_escolhido = input(
+            '\nEscolha um indice para tocar a musica de um cliente ou (N) para voltar ao menu.\nEscolha um indice:  ').lower()
+        if indice_escolhido == "n":
+            continue
+        elemento_escolhido = devices.split('\n')[int(indice_escolhido) - 1]
+        elemento_dividido = elemento_escolhido.split("Musica: ")
+        musica = elemento_dividido[1]
+        if play:
+            play = False
+            print("Digite Play ou '1' no menu para tocar a musica.")
+        else:
+            print("Digite Play ou '1' no menu para tocar a musica.")
+            # elif comando == "play(msc)_ip" or comando == "11":
     else:
         print("Invalid command.")
 
